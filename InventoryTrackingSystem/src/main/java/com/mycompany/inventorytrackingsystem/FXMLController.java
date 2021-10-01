b7515c5064060e2588025bf5a20076980ac96549
trevor-evans-owatc <50092148+trevor-evans-owatc@users.noreply.github.com>
2021-09-30 18:44:45 -0600

    First commit for assigned project

---
+++ InventoryTrackingSystem/src/main/java/com/mycompany/inventorytrackingsystem/FXMLController.java
@@ -0,0 +1,30 @@
+package com.mycompany.inventorytrackingsystem;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+public class FXMLController implements Initializable {
+    
+    @FXML
+    private Label label;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+    /**@FXML
+    public void exitBut(Actionevent e){
+        System.exit();
+    }**/
+}
